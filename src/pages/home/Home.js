755128d7cd0bf8d7286343b90f44dfdbd5c905b7
Bruce Ambundo <bruceambundo@gmail.com>
2025-08-10 19:49:18 +0300

    Home page responsive

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,45 +10,36 @@ import Date from '../../components/homecomponents/date/Date'
 
 export default function Home() {
   return (
-    <div>
-      <div className='home-page'>
+    <div className='home-page'>
+      <div className='home-one'>
         <div>
-          <div className='home-one'>
-            <div>
-              <div className=''>
-                <BigDiv />
-              </div>
+          <BigDiv />
 
-              <div className='d-flex align-items-center gap-2'>
-                <Payment />
-                <Kasnei />
-              </div>
-            </div>
-
-            <div className='d-flex flex-column justify-content-center'>
-              <div 
-                className='d-flex align-items-center '
-                style={{ gap: "80px" }}
-              >
-                <p className='tracking-text'>Tracking</p>
-
-                <AlertCircle
-                  size={25}
-                  className=''
-                  style={{ color: "rgba(161, 161, 161)" }}
-                />
-              </div>
-              <TrackingDiv />
-            </div>
+          <div className='d-flex align-items-center mt-2 gap-2'>
+            <Payment />
+            <Kasnei />
           </div>
         </div>
+        
+        <div>
+          <div className='d-flex align-items-center gap-5'>
+            <p className='tracking-text'>Tracking</p>
 
+            <AlertCircle
+              size={25}
+              className=''
+              style={{ color: "rgba(161, 161, 161)" }}
+            />
+          </div>
+          <TrackingDiv />
+        </div>
       </div>
-    
-      <div className='home-two mt-4 mb-2 d-flex align-items-center justify-content-center gap-3 '>
-        <Date />
+
+      <div className='home-two'>
         <Music />
+        <Date />
       </div>
+
     </div>
   )
 }
